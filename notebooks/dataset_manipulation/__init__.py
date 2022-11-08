c882dc16c7370dfe4bae25d116eeac06a4f067d9
adelezaini <59051647+adelezaini@users.noreply.github.com>
2022-11-08 20:50:01 +0100

    Create packages

---
+++ notebooks/dataset_manipulation/__init__.py
@@ -0,0 +1,2 @@
+from .coordinate_operations import convert360_180, convert180_360, convert_lsmcoord, convert_to_lsmcoord, xr_prod_along_dim
+from .fit import polynomial_fit, gaussian_fit
--- notebooks/dataset_manipulation/coordinate_operations.py
+++ notebooks/dataset_manipulation/coordinate_operations.py
@@ -6,7 +6,6 @@ import netCDF4 as nc
 import xarray as xr; xr.set_options(display_style='html')
 import pandas as pd
 import os
-from scipy.special import erf
 
 
 ################ Operations over coordinates
@@ -47,7 +46,7 @@ def convert_lsmcoord(ds):
     #Rename coord
     _ds = _ds.rename({'lsmlat':'lat','lsmlon':'lon'})
     return _ds
-    #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
+#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
 def convert_to_lsmcoord(ds):
     """ Convert lon-lat into Land Surface Model coordinates. """
     print("Allert convert_to_lsmcoord! Changing from coord to dim means loosing information and changing from values to indexes")
@@ -75,41 +74,3 @@ def xr_prod_along_dim(ds, weights, dim):
     ds_weighted = ds_weighted.transpose(*old_order)
 
     return ds_weighted
-    
-    
-################ Fitting in Xarray
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
-def polynomial_fit(ds, dim, deg=3):
-    out = ds.polyfit(dim=dim, deg=deg, full=True)
-    polyfit = xr.polyval(coord=ds[dim], coeffs=out.polyfit_coefficients).rename('polyfit')
-    return polyfit
-
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
-def gaussian_fit(ds, dim, p0={'A':45, 'xc':55, 'sig':5}, asym_gauss=False, fixed_extremes=False):
-    
-    names=None
-    
-    def gaussian(x, A, xc, sig):
-        return A*np.exp(-np.power((x - xc),2.) / (2 * np.power(sig,2.)))
-    
-    def asym_gaussian(x, *p):
-        amp = (p[0] / (p[2] * np.sqrt(2 * np.pi)))
-        spread = np.exp((-(x - p[1]) ** 2.0) / (2 * p[2] ** 2.0))
-        skew = (1 + erf((p[3] * (x - p[1])) / (p[2] * np.sqrt(2))))
-        return amp * spread * skew
-    
-    if asym_gauss:
-        gaussian=asym_gaussian
-        p0['p3']=1
-        names=['A', 'xc', 'sig', 'p3']
-        
-    if fixed_extremes==True:
-        sigma = np.ones(len(ds[dim].values))
-        sigma[[0, -1]] = 0.01
-        fit_params=ds.curvefit(coords=dim, func=gaussian, p0=p0, param_names=names, kwargs={'sigma': sigma})
-    else:
-        fit_params=ds.curvefit(coords=dim, func=gaussian, p0=p0, param_names=names)#, kwargs={'sigma': sigma})
-    popt=fit_params.curvefit_coefficients # NO! .values
-    gaussfit = gaussian(ds[dim], *popt.T).drop('param').rename('gaussfit')
-    return gaussfit
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
